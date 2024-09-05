a35302e796cbbfdca3150537e357f154560399b8
Наталья Бальчугова <balchugova98@bk.ru>
2024-09-05 21:58:32 +0700

    Edit test_short_phrase

--- pythonProject/test_short_phrase.py
+++ pythonProject/test_short_phrase.py
@@ -1,4 +1,6 @@
 def test_short_phrase():
+    print("Enter a phrase shorter than 15 characters")
     phrase = input("Set a phrase: ")
     assert len(phrase) == 15
-    print("Success")
+    if len(phrase) == 15:
+        print("Success")
