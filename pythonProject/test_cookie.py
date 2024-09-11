12b8f8f0b06928a106166eddb35418f956d5b61e
Наталья Бальчугова <balchugova98@bk.ru>
2024-09-12 00:50:55 +0700

    Create test_header.py

--- pythonProject/test_cookie.py
+++ pythonProject/test_cookie.py
@@ -6,7 +6,7 @@ url = 'https://playground.learnqa.ru/api/homework_cookie'
 def test_cookie():
     response = requests.get(url)
     cookie = response.cookies
-    # Ввод куки
+    # Вывод куки
     print(cookie)
     # Проверка значения куки
     assert "HomeWork" in cookie
