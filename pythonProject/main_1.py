d28f2b26eb81f1210f2e4a139a90e61a90473b99
Наталья Бальчугова <balchugova98@bk.ru>
2024-09-22 23:29:44 +0700

    Create base_case, assertions, test_user_auth.py

--- pythonProject/main_1.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-responce = requests.get("https://playground.learnqa.ru/api/hello/2")
-print(responce.text)
--- pythonProject/test_short_phrase.py
+++
@@ -1,6 +0,0 @@
-def test_short_phrase():
-    print("Enter a phrase shorter than 15 characters")
-    phrase = input("Set a phrase: ")
-    assert len(phrase) == 15
-    if len(phrase) == 15:
-        print("Success")
